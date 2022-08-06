bcf8d8169ab129dc8d5ec36c409dffbe08b4b0c0
sarthinigam123 <sarthinigam123@gmail.com>
2022-08-06 16:07:31 +0530

    removed comment from postsclient and refactored getpostsbyid

--- app/src/main/java/api_capstone/posts/GetPostsByIDTests.java
+++ app/src/main/java/api_capstone/posts/GetPostsByIDTests.java
@@ -1,20 +1,25 @@
 package api_capstone.posts;
 
+import api_capstone.posts.create.CreatePostResponse;
+import api_capstone.posts.services.PostsServices;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class GetPostsByIDTests {
+    private PostsServices postsService;
+    @BeforeClass
+    public void beforeClass(){
+        postsService=new PostsServices();
+    }
 
     @Test
-    public void shouldGetPostsByID() {
-
-        given()
-                .header("app-id", "62ec44a25c53f4761649efac")
-                .when()
-                .get("https://dummyapi.io/data/v1/post/62ecc3ed98146a9c9a3e004a")
-                .then()
-                .statusCode(200)
-                .log().body();
+    public void shouldGetPostsById(){
+        String id="62ecc3ed98146a9c9a3e004a";
+        CreatePostResponse postResponse= postsService.getPost(id);
+        Assert.assertEquals(postResponse.getText(),"Dog");
+        Assert.assertEquals(postResponse.getStatusCode(),200);
     }
 }
